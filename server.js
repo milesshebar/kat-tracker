0b331b48b25823d81d4f6a952c1d5fa6885427ca
Glitch (hello-express) <none>
2020-04-28 03:57:24 +0000

    🎿💪 Checkpoint ./views/index.html:541641/3406 ./server.js:541641/2107

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-// init project
 const express = require("express");
-var parseString = require("xml2js").parseString;
 const request = require("request");
 const fs = require("fs");
-const util = require('util')
-const DOMParser = require("xmldom").DOMParser;
-
 
 const options = {
   method: "GET",
-  uri: "http://knox.ecolane.com/mde.php?q=vehicle_live"
+  uri: process.env.KAT_FEED
 };
+
 
 const app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 function KATkml() {
-  request(options, function(err, res, body) {
+  request(options, function putTemp(err, res, body) {
     if (err) throw err;
-    var xml = body;
+    var kml = body;
 
-    parseString(xml,function(err, result) {
-      var jsoniem = JSON.stringify(result);
-      result.kml.Document[0].Placemark.forEach(function (el) {
-        if (el.name == '124 (MTV-Gamb Evening)' || el.name == '143 (MTV-Gamb Day)') {
-          var data = el.Point[0].coordinates[0];
-          io.emit("shuttle", data);
-        }      
-      });
+    
+    fs.writeFile(__dirname + "/public/tmp.kml", kml, err => {
+      io.emit("kml", {});
     });
-  })};
+  });
+}
 
 setInterval(KATkml, 10000);
 
 io.on("connection", function(socket) {
-  console.log("connected");
+  socket.emit("kml", {});
 });
 
 app.use(express.static("public"));
@@ -44,6 +36,6 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
